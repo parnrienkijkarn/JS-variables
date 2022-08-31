547fa4ea5be122dcce9061facdda13c0c422d5b0
Paul Ren <pause.forever@gmail.com>
2022-08-31 19:55:36 +0700

    add exercise in JS Variables

---
+++ ex-1and2.js
@@ -0,0 +1,13 @@
+//Exercise #1: Variable Creation Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Krauy Bang Kruai";
+
+const restaurantaName = "Chef's Table by Chef James"
+const totalBill = 15000;
+const totalRestaurant = 1500;
+
+//Exercise #2: Variable Reassignment Start coding here
+address = "(970)878-53292400 County Rd #4Meeker, Colorado(C0), 81641";
+totalBill = 5000;
+totalRestaurant = 2500;
---
+++ ex-3.js
@@ -0,0 +1,4 @@
+let celcius = 30;
+let fahrenheit = (celcius * 1.8) + 32;
+
+console.log(fahrenheit);
